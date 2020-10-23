cfe3b0eff2a376dcbe5148426692134e5249b47f
choisungwook <kgg1959@naver.com>
2020-10-23 10:29:15 +0900

    Add html-loader 설정, test.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,31 @@
 const path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'production',
   entry: './src/index.js',
+  optimization: {
+    minimize: true
+  },
+  plugins: [new HtmlWebpackPlugin({
+    filename: "test.html",
+    template: "./src/test.html",
+    minify: false,
+    inject: false
+  })],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        use: [
+          "html-loader?minimize=false",
+
+        ]
+      },
+    ],
   },
 };
