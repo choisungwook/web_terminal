bbcc7378463a48ed1d9f93f8b340b5096866e61b
choisungwook <kgg1959@naver.com>
2020-10-23 18:43:31 +0900

    Add websocket attach

--- src/index.js
+++ src/index.js
@@ -3,13 +3,12 @@ import { AttachAddon } from 'xterm-addon-attach';
 import './xterm.css'
 
 const term = new Terminal();
-const socket = new WebSocket('ws://localhost:3100');
+const socket = new WebSocket('ws://localhost:8010');
 const attachAddon = new AttachAddon(socket);
 
 // Attach the socket to term
 socket.onopen = function() {
-    console.log("success");
-    // term.loadAddon(attachAddon);
+    term.loadAddon(attachAddon);    
 };
 
 socket.onerror = function(e) {
