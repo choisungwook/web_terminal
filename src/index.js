cc54c896bc62ee39f1050309e0a7e9e28dd3b0c2
choisungwook <kgg1959@naver.com>
2020-10-23 17:54:33 +0900

    Add 웹소켓 연결

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import { Terminal } from 'xterm';
+import { AttachAddon } from 'xterm-addon-attach';
 import './xterm.css'
 
 const term = new Terminal();
+const socket = new WebSocket('ws://localhost:3100');
+const attachAddon = new AttachAddon(socket);
+
+// Attach the socket to term
+socket.onopen = function() {
+    console.log("success");
+    // term.loadAddon(attachAddon);
+};
+
+socket.onerror = function(e) {
+    console.log(e);
+};
 
 term.open(document.getElementById('xterm-container'));
-
-term.write("hello world");
